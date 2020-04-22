daed927c5a30cb0d15e23400b60a9c13874f2996
yeonggyulim <lyg940128@gmail.com>
2020-04-22 13:44:21 +0900

    [feat] Footer 컴포넌트 모든 페이지에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,45 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import { Main, Information, Career, Map, Contact } from "./pages";
+// import Header from './components/common/Header';
+import Footer from './components/common/Footer';
 import "./App.css";
 
 const App = () => {
-  return (
-    <>
-      <Switch>
-        <Route path="/information/map" component={Map} />
-        <Route path="/information/career" component={Career} />
-        <Route path="/information" exact component={Information} />
-        {/* TODO: change name to buddha-statue, antique, character */}
-        <Route path="/processing/probuddha" />
-        <Route path="/processing/probequest" />
-        <Route path="/processing/procha" />
-        <Route path="/processing" />
-        <Route path="/gallery/art-exhibition" />
-        <Route path="/gallery/diorama" />
-        <Route path="/gallery/antique-reproduction" />
-        <Route path="/gallery/cultural-assets" />
-        <Route path="/gallery/statue" />
-        <Route path="/gallery/sculpture" />
-        <Route path="/gallery/character" />
-        <Route path="/gallery/model" />
-        <Route path="/gallery" />
-        <Route path="/buddhism-art/buddha-statue" />
-        <Route path="/buddhism-art/buddhist-sculpture" />
-        <Route path="/buddhism-art/buddhist-architecture" />
-        <Route path="/buddhism-art/buddhist-goods" />
-        <Route path="/buddhism-art/3d-data" />
-        <Route path="/buddhism-art" />
-        <Route path="/contact" component={Contact} />
-        <Route path="/" exact component={Main} />
-        {/* TODO: Error Page */}
-        <Route />
-      </Switch>
-    </>
-  );
+    return (
+        <div id='container'>
+            <Switch>
+                <Route path="/information/map" component={Map} />
+                <Route path="/information/career" component={Career} />
+                <Route path="/information" exact component={Information} />
+                {/* TODO: change name to buddha-statue, antique, character */}
+                <Route path="/processing/probuddha" />
+                <Route path="/processing/probequest" />
+                <Route path="/processing/procha" />
+                <Route path="/processing" />
+                <Route path="/gallery/art-exhibition" />
+                <Route path="/gallery/diorama" />
+                <Route path="/gallery/antique-reproduction" />
+                <Route path="/gallery/cultural-assets" />
+                <Route path="/gallery/statue" />
+                <Route path="/gallery/sculpture" />
+                <Route path="/gallery/character" />
+                <Route path="/gallery/model" />
+                <Route path="/gallery" />
+                <Route path="/buddhism-art/buddha-statue" />
+                <Route path="/buddhism-art/buddhist-sculpture" />
+                <Route path="/buddhism-art/buddhist-architecture" />
+                <Route path="/buddhism-art/buddhist-goods" />
+                <Route path="/buddhism-art/3d-data" />
+                <Route path="/buddhism-art" />
+                <Route path="/contact" component={Contact} />
+                <Route path="/" exact component={Main} />
+                {/* TODO: Error Page */}
+                <Route />
+            </Switch>
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,16 @@
-// import React from 'react';
+import React from 'react';
 
 const Footer = () => {
-    
+    return (
+        <footer>
+            <p>
+                JeonHeung
+            </p>
+            <p>
+                Total Art Company
+            </p>
+        </footer>
+    );
 }
 
 export default Footer;
