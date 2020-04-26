05be90546578aa542a210632a9b6a5f36e991c06
yeonggyulim <lyg940128@gmail.com>
2020-04-26 19:46:43 +0900

    Update Footer

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p>
+            <p className='title'>
                 전흥공예
             </p>
-            <p style={{}}>
+            <p className='copyright'>
                 COPYRIGHT &copy; {new Date().getFullYear()} 田興工藝 ALL RIGHTS RESERVED
             </p>
         </footer>
