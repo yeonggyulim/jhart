8bc5cb94aebfb890dfa582f33260b1107b82778b
yeonggyulim <lyg940128@gmail.com>
2020-05-03 19:18:59 +0900

    Update Footer

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 const Footer = () => {
-    return (
-        <footer>
-            <p className='title'>
-                전흥공예
-            </p>
-            <p className='copyright'>
-                COPYRIGHT &copy; {new Date().getFullYear()} 田興工藝 ALL RIGHTS RESERVED
-            </p>
-        </footer>
-    );
-}
+  return (
+    <footer>
+      <p className="title">전흥공예</p>
+      <p className="information">대표: 임병시 Tel: 031-989-2344</p>
+      <p className="copyright">
+				COPYRIGHT &copy; {new Date().getFullYear()} 田興工藝 ALL RIGHTS RESERVED
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
