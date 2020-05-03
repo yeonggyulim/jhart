879bbc15c43ea2542e72d42dc10fa025529700f2
yeonggyulim <lyg940128@gmail.com>
2020-05-03 19:19:48 +0900

    Update Navigation with links

--- src/containers/navigation/NavigationContainer.tsx
+++ src/containers/navigation/NavigationContainer.tsx
@@ -6,19 +6,33 @@ import { Navigation } from '../../layouts';
 const NavigationContainer = () => {
   const navigations = [
     [],
-    ['회사 소개', '찾아오시는 길', '연혁'],
-    ['대불 조성', '유지 보수', '캐릭터'],
     [
-      '전시 미술',
-      '디오라마',
-      '유물 복제',
-      '문화재',
-      '인물',
-      '조형물',
-      '캐릭터',
-      '모형',
+      { title: '회사 소개', link: '/information' },
+      { title: '찾아오시는 길', link: '/information/map' },
+      { title: '연혁', link: '/information/career' },
     ],
-    ['대불 조성', '불교 조각', '불교 건축', '불교 용품', '3D Data'],
+    [
+      { title: '대불 조성', link: '/processing/probuddha' },
+      { title: '유지 보수', link: '/processing/probequest' },
+      { title: '캐릭터', link: '/procssing/procha' },
+    ],
+    [
+      { title: '전시 미술', link: '/modeling/art-exhibition' },
+      { title: '디오라마', link: '/modeling/diorama' },
+      { title: '유물 복제', link: '/modeling/antique-reproduction' },
+      { title: '문화재', link: '/modeling/cultural-assets' },
+      { title: '인물', link: '/modeling/statue' },
+      { title: '조형물', link: '/modeling/sculpture' },
+      { title: '캐릭터', link: '/modeling/character' },
+      { title: '모형', link: '/modeling/model' },
+    ],
+    [
+      { title: '대불 조성', link: '/buddhism-art/buddha-statue' },
+      { title: '불교 조각', link: '/buddhism-art/buddhist-sculpture' },
+      { title: '불교 건축', link: '/buddhism-art/buddhist-architecture' },
+      { title: '불교 용품', link: '/buddhism-art/buddhist-goods' },
+      { title: '3D Data', link: '/buddhism-art/3d-data' },
+    ],
     [],
   ];
 
--- src/layouts/navigation/Navigation.tsx
+++ src/layouts/navigation/Navigation.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 type NavigationProps = {
-	navigations: string[];
+	navigations: {
+		title: string;
+		link: string;
+	}[];
 };
 
 const Navigation = ({ navigations }: NavigationProps) => {
+  const history = useHistory();
   return (
     <div className="outer-layout navigation">
       <div className="inner-layout navigation">
         <nav>
           {navigations.map((navigation, i) => {
-            return <a key={i}>{navigation}</a>;
+            return (
+              <a key={i} onClick={() => history.push(navigation.link)}>
+                {navigation.title}
+              </a>
+            );
           })}
         </nav>
       </div>
