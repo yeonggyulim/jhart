44b7bae9df87a4dc4e9b34c8af935bf4554cfcc0
yeonggyulim <lyg940128@gmail.com>
2020-06-18 21:51:06 +0900

    [backend] 몽고db내에 post 생성, 조회 api 수정

--- jhart-backend/src/api/posts/index.ts
+++ jhart-backend/src/api/posts/index.ts
@@ -3,10 +3,11 @@ import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
-posts.get('/', postsCtrl.list);
+
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+posts.get('/:categories', postsCtrl.list);
+posts.get('/:categories/:id', postsCtrl.read);
+posts.delete('/:categories/:id', postsCtrl.remove);
+posts.patch('/:categories/:id', postsCtrl.update);
 
 export default posts;
--- jhart-backend/src/api/posts/posts.ctrl.ts
+++ jhart-backend/src/api/posts/posts.ctrl.ts
@@ -1,4 +1,5 @@
 import { Context } from 'koa';
+import Post from '../../models/post';
 let postId = 1; // id의 초깃값
 
 // posts 배열 초기 데이터
@@ -12,42 +13,61 @@ const posts = [
 
 /* 포스트 작성
 POST /api/posts
-{ title, body }
+{ 
+    title: '제목',
+    body: '내용',
+    categories: '카테고리'
+}
 */
-export const write = (ctx: any) => {
+export const write = async (ctx: any) => {
   // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
-  const { title, body } = ctx.request.body;
-  postId += 1; // 기존 postId 값에 1 더함
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.body = post;
+  const { title, body, categories } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    categories,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
-/* 포스트 목록 조회
-GET /api/posts
+/* 해당 카테고리의 포스트 목록 조회
+GET /api/posts/:categories
 */
-export const list = (ctx: Context) => {
-  ctx.body = posts;
+
+export const list = async (ctx: Context) => {
+  const { categories } = ctx.params;
+  try {
+    const posts = await Post.find({ categories }).exec();
+    if (!posts) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 /* 특정 포스트 조회
-GET /api/posts/:id
+GET /api/posts/:categories/:id
 */
-export const read = (ctx: Context) => {
-  const { id } = ctx.params;
-  // 주어진 id 값으로 포스트 찾음
-  // 파라미터로 받아온 값: 문자열
-  // p.id: 숫자
-  const post = posts.find((p) => p.id.toString() === id);
-  //  포스트 없으면 오류 반환
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
+export const read = async (ctx: Context) => {
+  const { categories, id } = ctx.params;
+  try {
+    const post = await Post.find({ categories, _id: id }).exec();
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  ctx.body = post;
 };
 
 /* 특정 포스트 제거
@@ -75,22 +95,22 @@ PATCH /api/posts/:id
 { title, body }
 */
 export const update = (ctx: any) => {
-    // PATCH 메소드는 주어진 필드만 교체
-    const { id } = ctx.params;
-    // 해당 id를 가진 post가 몇 번째인지 확인 
-    const index = posts.findIndex(p => p.id.toString() === id);
-    // 포스트 없으면 오류 반환
-    if (index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.',
-        };
-        return;
-    }
-    // 기존 값에 정보 덮어 씌움
-    posts[index] = {
-        ...posts[index],
-        ...ctx.request.body,
+  // PATCH 메소드는 주어진 필드만 교체
+  const { id } = ctx.params;
+  // 해당 id를 가진 post가 몇 번째인지 확인
+  const index = posts.findIndex((p) => p.id.toString() === id);
+  // 포스트 없으면 오류 반환
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: '포스트가 존재하지 않습니다.',
     };
-    ctx.body = posts[index];
+    return;
+  }
+  // 기존 값에 정보 덮어 씌움
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body,
+  };
+  ctx.body = posts[index];
 };
