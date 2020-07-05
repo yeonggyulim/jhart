740058f839ccbff972accf4947610489a0d5dd8e
yeonggyulim <lyg940128@gmail.com>
2020-07-05 16:25:46 +0900

    [frontend] Auth api 타입 및 함수 작성

---
+++ jhart-frontend/src/lib/api/auth.ts
@@ -0,0 +1,17 @@
+import client from './client';
+
+export type AuthType = {
+	username: string;
+	password: string;
+};
+
+// 로그인
+export const login = ({ username, password }: AuthType) =>
+	client.post('/api/auth/login', { username, password });
+
+// 회원가입
+export const register = ({ username, password }: AuthType) =>
+	client.post('/api/auth/register', { username, password });
+
+// 로그인 상태 확인
+export const check = () => client.get('/api/auth/check');
