4d60431a0c559b61463aa76f435ad8767d39e0cc
yeonggyulim <lyg940128@gmail.com>
2020-07-08 19:19:49 +0900

    [frontend] 로그인 구현

--- jhart-frontend/src/containers/auth/LoginForm.tsx
+++ jhart-frontend/src/containers/auth/LoginForm.tsx
@@ -3,12 +3,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../modules';
 import { authActions, KeyType } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { userActions } from '../../modules/user';
+import { useHistory } from 'react-router-dom';
 
 const LoginForm = () => {
+	const history = useHistory();
 	const dispatch = useDispatch();
-	const { form } = useSelector(({ auth }: RootState) => ({
-		form: auth.login,
-	}));
+	const { form, auth, authError, user } = useSelector(
+		({ auth, user }: RootState) => ({
+			form: auth.login,
+			auth: auth.auth,
+			authError: auth.authError,
+			user: user.user,
+		}),
+	);
 	// 인풋 변경 이벤트 핸들러
 	const onChange = (e: React.FormEvent<HTMLInputElement>) => {
 		const { value, name } = e.currentTarget;
@@ -24,13 +32,32 @@ const LoginForm = () => {
 	// 폼 등록 이벤트 핸들러
 	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		// 구현 예정
+		const { username, password } = form;
+		dispatch(authActions.login({ username, password }));
 	};
 
-	// 컴포넌트가 청므 렌더링될 때 form 초기화
+	// 컴포넌트가 처음 렌더링될 때 form 초기화
 	useEffect(() => {
 		dispatch(authActions.initializeForm('login'));
 	}, [dispatch]);
+
+	useEffect(() => {
+		if (authError) {
+			console.log('오류 발생');
+			console.log(authError);
+			return;
+		}
+		if (auth) {
+			console.log('로그인 성공');
+			dispatch(userActions.check());
+		}
+	}, [auth, authError, dispatch]);
+
+	useEffect(() => {
+		if (user) {
+			history.push('/');
+		}
+	}, [history, user]);
 
 	return (
 		<AuthForm
