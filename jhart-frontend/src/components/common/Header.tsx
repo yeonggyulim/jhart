5f62f33e6f348402af72d1a3d32427fa8554d591
yeonggyulim <lyg940128@gmail.com>
2020-07-09 19:40:16 +0900

    [frontend] 헤더에 로그인 아이콘과 지도 아이콘 추가

--- jhart-frontend/src/components/common/Header.tsx
+++ jhart-frontend/src/components/common/Header.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Logo, Gnb } from '..';
+import { FaRegUser } from 'react-icons/fa';
+import { GoLocation } from 'react-icons/go';
 
 const Header = () => {
-    return (
-        <div className="outer-layout header">
-            <div className="inner-layout header">
-                <div className="header--left">
-                    <Logo />
-                </div>
-                <div className="header--center">
-                    <Gnb />
-                </div>
-                <div className="header--right" />
-            </div>
-        </div>
-    );
+	return (
+		<div className="outer-layout header">
+			<div className="inner-layout header">
+				<div className="header--left">
+					<Logo />
+				</div>
+				<div className="header--center">
+					<Gnb />
+				</div>
+				<div className="header--right">
+					<Link to="/login">
+						<FaRegUser />
+					</Link>
+					<Link to="/information/map">
+						<GoLocation />
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default React.memo(Header);
