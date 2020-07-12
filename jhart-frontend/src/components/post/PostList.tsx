67b39f78559ab96bb1a5ba572e9b826c63c52c2f
yeonggyulim <lyg940128@gmail.com>
2020-07-12 21:54:39 +0900

    [frontend] 포스트 관련 컴포넌트 추가

---
+++ jhart-frontend/src/components/post/PostList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostList = () => {
+	return <div>포스트 리스트</div>;
+};
+
+export default PostList;
---
+++ jhart-frontend/src/components/post/PostWrite.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostWrite = () => {
+	return <div>글쓰기</div>;
+};
+
+export default PostWrite;
