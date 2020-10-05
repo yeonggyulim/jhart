606c8b73c13af21e473dc2bfaa17ad7ffced11c3
yeonggyulim <lyg940128@gmail.com>
2020-10-05 18:02:08 +0900

    [frontend] webpack setting

--- jhart-frontend/src/index.tsx
+++ jhart-frontend/src/index.tsx
@@ -1,1 +1,10 @@
 import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement: HTMLElement = document.getElementById('app');
+
+
+ReactDOM.render(
+    <div></div>,
+    document.getElementById('root'),
+);
--- jhart-frontend/webpack.config.ts
+++ jhart-frontend/webpack.config.ts
@@ -1,8 +1,20 @@
 import * as path from 'path';
+import * as webpack from 'webpack';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+
+interface Configuration extends webpack.Configuration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const WDS_PORT = 8080;
+
+const mode = process.env.NODE_ENV as 'development' || 'production';
+
+const config: Configuration = {
+  mode,
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, 'public/dist'),
@@ -12,8 +24,9 @@ module.exports = {
   devtool: 'inline-source-map',
 
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
+
   module: {
     rules: [{
       test: /\.(js|ts)$/,
@@ -32,10 +45,17 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			title: '',
-			filename: './index.html',
-		}),
-  ]
+    new HtmlWebpackPlugin({
+      title: 'JHArt',
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+
+  devServer: {
+    overlay: true,
+    port: WDS_PORT,
+  }
 }
 
+export default config;
