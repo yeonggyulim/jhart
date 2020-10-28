aba7e7ae950c3e9065d331d13002752f7d0afc15
yeonggyulim <lyg940128@gmail.com>
2020-10-28 13:47:43 +0900

    [backend] provide static file with koa-static

--- jhart-backend/src/index.ts
+++ jhart-backend/src/index.ts
@@ -3,6 +3,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 import api from './api';
 import jwtMiddleware from './lib/jwtMiddleware';
@@ -34,6 +37,17 @@ app.use(jwtMiddleware);
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
+
+// 정적 파일 제공(build 결과물 serve)
+const buildDirectory = path.resolve(__dirname, '../../jhart-frontend/public/dist');
+app.use(serve(buildDirectory));
+app.use(async ctx => {
+  // Not Found 이고, 주소가 /api로 시작하지 않는 경우
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    // index.html 내용을 반환
+    await send(ctx, '.index.html', { root: buildDirectory });
+  }
+});
 
 // PORT가 지정되어 있지 않다면 4000 사용
 const port = PORT || 4000;
