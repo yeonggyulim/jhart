a63935c4fd26a9b6523f3c3299a00d712918f0ef
yeonggyulim <lyg940128@gmail.com>
2020-10-28 20:23:42 +0900

    [frontend] add PrivateRouter

---
+++ jhart-frontend/src/router/PrivateRouter.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface IPrivateRouterProps {}
+
+const PrivateRouter: React.FunctionComponent<IPrivateRouterProps> = (props) => {
+	return <div></div>;
+};
+
+export default PrivateRouter;
--- jhart-frontend/src/router/index.tsx
+++ jhart-frontend/src/router/index.tsx
@@ -1,2 +1,14 @@
 import * as React from 'react';
-import { }
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+interface IRouterProps {}
+
+const Router: React.FunctionComponent<IRouterProps> = (props) => {
+	return (
+		<BrowserRouter>
+			<Switch>{/* <Route  */}</Switch>
+		</BrowserRouter>
+	);
+};
+
+export default Router;
