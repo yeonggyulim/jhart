14279d11d9c4f40b4523d638033d04c973ab5c29
yeonggyulim <lyg940128@gmail.com>
2020-11-11 14:07:40 +0900

    [frontend] add components for header

---
+++ jhart-frontend/src/components/Header.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface IHeaderProps {}
+
+export function Header(props: IHeaderProps) {
+	return <div></div>;
+}
---
+++ jhart-frontend/src/containers/CategoryContainer.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export interface ICategoryContainerProps {}
+
+export function CategoryContainer(props: ICategoryContainerProps) {
+	return <div></div>;
+}
