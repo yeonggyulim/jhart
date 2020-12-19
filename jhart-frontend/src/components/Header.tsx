44f31020191ab13a9e236dc28463b8121b47ac4d
yeonggyulim <lyg940128@gmail.com>
2020-12-19 23:38:35 +0900

    [frontend] open-color 추가 및 헤더 스타일 추가

--- jhart-frontend/src/components/Header.tsx
+++ jhart-frontend/src/components/Header.tsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import oc from 'open-color';
 
 const HeaderBlock = styled.div`
-	color: red;
+	/* 레이아웃 */
+	display: flex;
+	position: fixed;
+	align-items: center;
+	justify-content: center;
+	height: 60px;
+	width: 100%;
+	top: 0px;
+	z-index: 5;
+
+	/* 색상 */
+	background: ${oc.indigo[6]};
+	color: white;
+	border-bottom: 1px solid ${oc.indigo[7]};
+	box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1), 0 3px 6px rgba(0, 0, 0, 0.2);
+
+	/* 폰트 */
+	font-size: 2.5rem;
 `;
 
 interface IHeaderProps {
