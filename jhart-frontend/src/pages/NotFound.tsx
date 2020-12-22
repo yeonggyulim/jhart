f3b7b2291bf1b6e6c3eb1d6d9252cb8dfcd250ad
yeonggyulim <lyg940128@gmail.com>
2020-12-23 00:34:07 +0900

    [frontend] 404 페이지 수정

--- jhart-frontend/src/pages/NotFound.tsx
+++ jhart-frontend/src/pages/NotFound.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import oc from 'open-color';
 
+const NotFoundBlock = styled.div`
+	font-size: 220px;
+	color: ${oc.gray[8]};
+`;
 interface INotFoundProps {
 	children?: React.ReactNode;
 }
 
 export const NotFound: React.FunctionComponent<INotFoundProps> = () => {
-	return <div>Page Not Found</div>;
+	return <NotFoundBlock>404</NotFoundBlock>;
 };
